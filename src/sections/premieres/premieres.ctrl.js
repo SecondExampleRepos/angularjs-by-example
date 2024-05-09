0caffc16f365cd71a9fa123a232a1f91469d4e16
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-09 13:43:01 -0700

    edit_file

--- src/sections/premieres/premieres.ctrl.js
+++ src/sections/premieres/premieres.ctrl.js
@@ -1,11 +1,13 @@
 'use strict';
+import angular from 'angular';
+import { IPageValues, IShow } from './interfaces';
 angular
     .module('app.core')
-    .controller('PremieresController', function($scope, shows, PageValues) {
+    .controller('PremieresController', function($scope: angular.IScope, shows: IShow[], PageValues: IPageValues) {
         //Set page title and description
         PageValues.title = "PREMIERES";
         PageValues.description = "Brand new shows showing this month.";
         //Setup view model object
-        var vm = this;
+        const vm = this;
         vm.shows = shows;
     });
