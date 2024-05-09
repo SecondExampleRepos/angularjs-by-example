1b2d8c000f405ddd3d954c4d9fc685e26819141f
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-09 13:43:18 -0700

    edit_file

--- src/assets/js/angular-truncate.js
+++ src/assets/js/angular-truncate.js
@@ -1,20 +1,20 @@
 angular.module('truncate', [])
-.filter('characters', function () {
-    return function (input, chars, breakOnWord) {
+.filter('characters', function (): (input: string, chars: number, breakOnWord: boolean) => string {
+    return function (input: string, chars: number, breakOnWord: boolean): string {
         if (isNaN(chars)) return input;
         if (chars <= 0) return '';
         if (input && input.length > chars) {
             input = input.substring(0, chars);
 
             if (!breakOnWord) {
-                var lastspace = input.lastIndexOf(' ');
+                let lastspace: number = input.lastIndexOf(' ');
                 //get last space
                 if (lastspace !== -1) {
                     input = input.substr(0, lastspace);
                 }
-            }else{
-                while(input.charAt(input.length-1) === ' '){
-                    input = input.substr(0, input.length -1);
+            } else {
+                while (input.charAt(input.length - 1) === ' ') {
+                    input = input.substr(0, input.length - 1);
                 }
             }
             return input + '...';
@@ -22,12 +22,12 @@ angular.module('truncate', [])
         return input;
     };
 })
-.filter('words', function () {
-    return function (input, words) {
+.filter('words', function (): (input: string, words: number) => string {
+    return function (input: string, words: number): string {
         if (isNaN(words)) return input;
         if (words <= 0) return '';
         if (input) {
-            var inputWords = input.split(/\s+/);
+            let inputWords: string[] = input.split(/\s+/);
             if (inputWords.length > words) {
                 input = inputWords.slice(0, words).join(' ') + '...';
             }
