ebb7ffd97e3b3069b85cb4bc0ef01fe68c0a89db
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-09 13:42:54 -0700

    edit_file

--- src/components/bar/bar.ctrl.js
+++ src/components/bar/bar.ctrl.js
@@ -1,7 +1,10 @@
 'use strict';
+import angular from 'angular';
+import { IPageValues } from './interfaces/IPageValues';
+import { IBarControllerScope } from './interfaces/IBarControllerScope';
 angular
     .module('app.core')
-    .controller('BarController', function($scope, PageValues) {
+    .controller('BarController', function($scope: IBarControllerScope, PageValues: IPageValues) {
         //Setup the view model object
         var vm = this;
         vm.data = PageValues;
