38ef126fd9c840ee35acb8a9d220e5362966daaa
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-09 13:43:09 -0700

    edit_file

--- src/sections/view/view.ctrl.js
+++ src/sections/view/view.ctrl.js
@@ -1,14 +1,27 @@
 'use strict';
+import angular from 'angular';
+interface PageValuesType {
+    title: string;
+    description: string;
+}
+interface ShowType {
+    original_name: string;
+    id: number;
+    cast: CastMember[];
+}
+interface CastMember {
+    // Define properties for CastMember as needed
+}
 angular
     .module('app.core')
-    .controller('ViewController', function($scope, $location, PageValues, show, ShowService, $routeParams) {
-        //Set page title and description
+    .controller('ViewController', function($scope: any, $location: any, PageValues: PageValuesType, show: ShowType, ShowService: any, $routeParams: any) {
+        // Set page title and description
         PageValues.title = "VIEW";
-        PageValues.description = "Overview, seasons & info for '" + show.original_name + "'.";
-        //Setup view model object
+        PageValues.description = `Overview, seasons & info for '${show.original_name}'.`;
+        // Setup view model object
         var vm = this;
         vm.show = show;
-        vm.setBannerImage = function() {
+        vm.setBannerImage = function(): { background: string, 'background-size': string, 'background-position': string } {
             return {
                 'background': 'url() no-repeat',
                 'background-size': '100%',
@@ -16,7 +29,7 @@ angular
             };
         };
         vm.show.cast = [];
-        ShowService.getCast(vm.show.id).then(function(response){
+        ShowService.getCast(vm.show.id).then(function(response: { cast: CastMember[] }) {
             vm.show.cast = response.cast;
         });
     });
