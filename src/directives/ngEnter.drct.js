d160dd295143eda9455a5313221632211c54794d
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-09 13:42:59 -0700

    edit_file

--- src/directives/ngEnter.drct.js
+++ src/directives/ngEnter.drct.js
@@ -1,13 +1,12 @@
 angular
     .module('app.core')
     .directive('ngEnter', ngEnter);
-
-function ngEnter() {
-    return function(scope, element, attrs) {
-        element.bind("keydown keypress", function(event) {
+function ngEnter(): angular.IDirective {
+    return function(scope: angular.IScope, element: JQLite, attrs: angular.IAttributes) {
+        element.bind("keydown keypress", function(event: KeyboardEvent) {
             if(event.which === 13) {
                 scope.$apply(function(){
-                    scope.$eval(attrs.ngEnter);
+                    scope.$eval(attrs.ngEnter as string);
                 });
                 event.preventDefault();
             }
