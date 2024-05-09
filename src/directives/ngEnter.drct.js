ea94c044125a48615e2f74a6412b39b7423ad187
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-09 13:30:15 -0700

    edit_file

--- src/directives/ngEnter.drct.js
+++ src/directives/ngEnter.drct.js
@@ -1,16 +1,24 @@
 angular
     .module('app.core')
     .directive('ngEnter', ngEnter);
-
-function ngEnter() {
-    return function(scope, element, attrs) {
-        element.bind("keydown keypress", function(event) {
-            if(event.which === 13) {
-                scope.$apply(function(){
-                    scope.$eval(attrs.ngEnter);
-                });
-                event.preventDefault();
-            }
-        });
+type NgEnterScope = {
+    $apply: (action: () => void) => void;
+    $eval: (expression: string) => void;
+};
+interface NgEnterAttributes extends angular.IAttributes {
+    ngEnter: string;
+}
+function ngEnter(): angular.IDirective {
+    return {
+        link: function(scope: NgEnterScope, element: JQLite, attrs: NgEnterAttributes) {
+            element.bind("keydown keypress", function(event: JQueryKeyEventObject) {
+                if(event.which === 13) {
+                    scope.$apply(() => {
+                        scope.$eval(attrs.ngEnter);
+                    });
+                    event.preventDefault();
+                }
+            });
+        }
     };
 }
