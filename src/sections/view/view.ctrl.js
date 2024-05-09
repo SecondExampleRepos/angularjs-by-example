2b780a73d1ffc640ae0bf76f821470dec9745a9e
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-09 13:30:30 -0700

    edit_file

--- src/sections/view/view.ctrl.js
+++ src/sections/view/view.ctrl.js
@@ -1,14 +1,18 @@
 'use strict';
+import angular from 'angular';
+import { IPageValues, IShow, ICastResponse } from './interfaces';
+
 angular
     .module('app.core')
-    .controller('ViewController', function($scope, $location, PageValues, show, ShowService, $routeParams) {
-        //Set page title and description
+    .controller('ViewController', ['$scope', '$location', 'PageValues', 'show', 'ShowService', '$routeParams', 
+        function($scope: angular.IScope, $location: angular.ILocationService, PageValues: IPageValues, show: IShow, ShowService: any, $routeParams: angular.route.IRouteParamsService) {
+        // Set page title and description
         PageValues.title = "VIEW";
-        PageValues.description = "Overview, seasons & info for '" + show.original_name + "'.";
-        //Setup view model object
+        PageValues.description = `Overview, seasons & info for '${show.original_name}'.`;
+        // Setup view model object
         var vm = this;
         vm.show = show;
-        vm.setBannerImage = function() {
+        vm.setBannerImage = function(): { background: string, 'background-size': string, 'background-position': string } {
             return {
                 'background': 'url() no-repeat',
                 'background-size': '100%',
@@ -16,7 +20,22 @@ angular
             };
         };
         vm.show.cast = [];
-        ShowService.getCast(vm.show.id).then(function(response){
+        ShowService.getCast(vm.show.id).then(function(response: ICastResponse){
             vm.show.cast = response.cast;
         });
-    });
+    }]);
+
+interface IShow {
+    id: number;
+    original_name: string;
+    cast: any[];
+}
+
+interface ICastResponse {
+    cast: any[];
+}
+
+interface IPageValues {
+    title: string;
+    description: string;
+}
