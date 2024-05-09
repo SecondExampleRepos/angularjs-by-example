6c8ccaf0f3d5b311f70492654ece16747aef303e
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-09 14:15:44 -0700

    edit_file

--- src/app.config.js
+++ src/app.config.js
@@ -1,12 +1,14 @@
 'use strict';
-angular
-    .module('app.config', [])
+import angular from 'angular';
+import { IHttpProvider, ILocationService, ILogService, IQService, IRootScopeService } from 'angular';
+
+module 'app.config' {
     .config(configs)
     .run(runs);
 
-function configs($httpProvider) {
-    var interceptor = function($location, $log, $q) {
-        function error(response) {
+function configs($httpProvider: IHttpProvider) {
+    const interceptor = function($location: ILocationService, $log: ILogService, $q: IQService) {
+        function error(response: { status: number }) {
             if (response.status === 401) {
                 $log.error('You are unauthorised to access the requested resource (401)');
             } else if (response.status === 404) {
@@ -16,18 +18,18 @@ function configs($httpProvider) {
             }
             return $q.reject(response);
         }
-        function success(response) {
+        function success(response: any) {
             //Request completed successfully
             return response;
         }
-        return function(promise) {
+        return function(promise: Promise<any>) {
             return promise.then(success, error);
         }
     };
     $httpProvider.interceptors.push(interceptor);
 }
 
-function runs($rootScope, PageValues) {
+function runs($rootScope: IRootScopeService, PageValues: { loading: boolean }) {
     $rootScope.$on('$routeChangeStart', function() {
         PageValues.loading = true;
     });
