9a3bcfea07a3e10862d88bd496871d428b6c1439
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-05-14 09:52:22 -0700

    edit_file

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,33 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var connect = require('gulp-connect');
-var uglify = require('gulp-uglify');
-var ngAnnotate = require('gulp-ng-annotate');
-var usemin = require('gulp-usemin');
-var rev = require('gulp-rev');
-var revOutdated  = require('gulp-rev-outdated');
-var minifyCss = require('gulp-minify-css');
-var ngHtml2Js = require("gulp-ng-html2js");
-var inject = require("gulp-inject");
-var clean = require('gulp-clean');
-var replace = require('gulp-replace');
-var gulpAngularExtender = require('gulp-angular-extender');
-
+import * as gulp from 'gulp';
+import concat from 'gulp-concat';
+import connect from 'gulp-connect';
+import uglify from 'gulp-uglify';
+import ngAnnotate from 'gulp-ng-annotate';
+import usemin from 'gulp-usemin';
+import rev from 'gulp-rev';
+import revOutdated from 'gulp-rev-outdated';
+import minifyCss from 'gulp-minify-css';
+import ngHtml2Js from "gulp-ng-html2js";
+import inject from "gulp-inject";
+import clean from 'gulp-clean';
+import replace from 'gulp-replace';
+import gulpAngularExtender from 'gulp-angular-extender';
 //Convert all HTML tpl files to Angular template module
 gulp.task('create-templates', function() {
     return gulp.src('./**/*.tpl.html')
         .pipe(ngHtml2Js({
             moduleName: "app.templates",
-            rename: function(url) {
-                return url.replace('src/', '');
-            }
+            rename: (url: string) => url.replace('src/', '')
         }))
         .pipe(concat("app.templates.js"))
         .pipe(gulp.dest("src/"));
 });
-
 //Inject the templates file into ./src/index.html to be picked up by usemin
 gulp.task('inject-templates', ['create-templates'], function() {
     return gulp.src('./src/index.html')
         .pipe(inject(gulp.src('./src/app.templates.js', {read: false}), {ignorePath: 'src', addRootSlash: false}))
         .pipe(gulp.dest('src/'));
 });
-
 //Minify, concatenate and version CSS and JS
 //Use ngAnnotate to take care of Angular inject issues
 gulp.task('usemin', ['inject-templates'], function() {
@@ -44,7 +39,6 @@ gulp.task('usemin', ['inject-templates'], function() {
         }))
         .pipe(gulp.dest('build/'));
 });
-
 //Add Angular module dependency for templates
 gulp.task('add-dependencies', ['usemin'], function () {
     gulp.src('build/app.min-*.js')
@@ -55,13 +49,11 @@ gulp.task('add-dependencies', ['usemin'], function () {
         }))
         .pipe(gulp.dest('build/'));
 });
-
 //Copy the asset files from src to build
 gulp.task('copy-asset-files', function() {
     gulp.src(['./src/assets/fonts/*']).pipe(gulp.dest('build/assets/fonts/'));
     gulp.src(['./src/assets/images/*']).pipe(gulp.dest('build/assets/images/'));
 });
-
 //Delete the temporary templates module file and remove the include from ./src/index.html
 gulp.task('clean', ['usemin'], function () {
     gulp.src('./src/app.templates.js', {read: false})
@@ -73,7 +65,6 @@ gulp.task('clean', ['usemin'], function () {
         .pipe(revOutdated(1)) // leave 1 latest asset file for every file name prefix.
         .pipe(clean());
 });
-
 //Start a web server on port 8282 to server the src app
 gulp.task('connect-dev', function() {
     connect.server({
@@ -81,7 +72,6 @@ gulp.task('connect-dev', function() {
         port: 8282
     });
 });
-
 //Start a web server on port 8283 to server the build app (You probably wouldn't use this server for production delivery)
 gulp.task('connect-prod', function() {
     connect.server({
@@ -89,13 +79,10 @@ gulp.task('connect-prod', function() {
         port: 8283
     });
 });
-
 gulp.task('watch', ['usemin'], function () {
     gulp.watch('src/**/*.js', ['usemin'])
 });
-
 //Build task which should be used to build the application to production (By a continuous integration server for example)
 gulp.task('build', ['create-templates', 'inject-templates', 'usemin', 'add-dependencies', 'copy-asset-files', 'clean', 'connect-prod']);
-
 //Default task which simply servers the source files
 gulp.task('default', ['connect-dev']);
