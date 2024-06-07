7831096c4bc021aad469d24c4e366a891db9d5c9
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-07 13:53:19 +0000

    edit_file

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
 'use strict';
-angular.module('app', ['ngRoute', 'ngAnimate', 'angularMoment', 'angular-preload-image', 'truncate', 'app.routes', 'app.core', 'app.services', 'app.config']);
-
+import * as angular from 'angular';
+import 'angular-route';
+import 'angular-animate';
+import 'angular-moment';
+import 'angular-preload-image';
+import 'truncate';
+import { appRoutes } from './app.routes';
+import { appCore } from './app.core';
+import { appServices } from './app.services';
+import { appConfig } from './app.config';
+angular.module('app', [
+  'ngRoute',
+  'ngAnimate',
+  'angularMoment',
+  'angular-preload-image',
+  'truncate',
+  appRoutes,
+  appCore,
+  appServices,
+  appConfig
+]);
