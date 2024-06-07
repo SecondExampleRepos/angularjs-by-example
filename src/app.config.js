3e5f053268a6b07e3f2bef22bb1f85377bf0aa45
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-07 13:17:25 +0000

    edit_file

--- src/app.config.js
+++ src/app.config.js
@@ -1,33 +1,41 @@
 'use strict';
+import * as angular from 'angular';
+import { IHttpProvider, IHttpResponse, IQService, ILocationService, ILogService, IRootScopeService } from 'angular';
+enum HttpStatus {
+    Unauthorized = 401,
+    NotFound = 404,
+    InternalServerError = 500
+}
+interface IPageValues {
+    loading: boolean;
+}
 angular
     .module('app.config', [])
     .config(configs)
     .run(runs);
-
-function configs($httpProvider) {
-    var interceptor = function($location, $log, $q) {
-        function error(response) {
-            if (response.status === 401) {
+function configs($httpProvider: IHttpProvider) {
+    const interceptor = ($location: ILocationService, $log: ILogService, $q: IQService) => {
+        function error(response: IHttpResponse<any>) {
+            if (response.status === HttpStatus.Unauthorized) {
                 $log.error('You are unauthorised to access the requested resource (401)');
-            } else if (response.status === 404) {
+            } else if (response.status === HttpStatus.NotFound) {
                 $log.error('The requested resource could not be found (404)');
-            } else if (response.status === 500) {
+            } else if (response.status === HttpStatus.InternalServerError) {
                 $log.error('Internal server error (500)');
             }
             return $q.reject(response);
         }
-        function success(response) {
+        function success(response: IHttpResponse<any>) {
             //Request completed successfully
             return response;
         }
-        return function(promise) {
+        return function(promise: ng.IPromise<any>) {
             return promise.then(success, error);
         }
     };
     $httpProvider.interceptors.push(interceptor);
 }
-
-function runs($rootScope, PageValues) {
+function runs($rootScope: IRootScopeService, PageValues: IPageValues) {
     $rootScope.$on('$routeChangeStart', function() {
         PageValues.loading = true;
     });
