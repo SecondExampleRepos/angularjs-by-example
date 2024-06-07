88ae1129b3c29daf36acfac7cf23cbaed018341a
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-07 13:17:31 +0000

    edit_file

--- src/app.routes.js
+++ src/app.routes.js
@@ -1,51 +1,65 @@
 'use strict';
-
+import angular from 'angular';
+import { IRouteProvider } from 'angular-route';
+enum Routes {
+    Home = '/',
+    Premieres = '/premieres',
+    Search = '/search',
+    SearchQuery = '/search/:query',
+    Popular = '/popular',
+    View = '/view/:id'
+}
+interface IShowService {
+    getPremieres(): Promise<any>;
+    getPopular(): Promise<any>;
+    get(id: string): Promise<any>;
+}
 angular
     .module('app.routes', ['ngRoute'])
     .config(config);
-
-function config ($routeProvider) {
-    $routeProvider.
-        when('/', {
+config.$inject = ['$routeProvider'];
+function config($routeProvider: IRouteProvider) {
+    $routeProvider
+        .when(Routes.Home, {
             templateUrl: 'sections/home/home.tpl.html',
             controller: 'HomeController as home'
         })
-        .when('/premieres', {
+        .when(Routes.Premieres, {
             templateUrl: 'sections/premieres/premieres.tpl.html',
             controller: 'PremieresController as premieres',
             resolve: {
-                shows: function(ShowService) {
+                shows: ['ShowService', (ShowService: IShowService) => {
                     return ShowService.getPremieres();
-                }
+                }]
             }
         })
-        .when('/search', {
+        .when(Routes.Search, {
             templateUrl: 'sections/search/search.tpl.html',
             controller: 'SearchController as search'
         })
-        .when('/search/:query', {
+        .when(Routes.SearchQuery, {
             templateUrl: 'sections/search/search.tpl.html',
             controller: 'SearchController as search'
         })
-        .when('/popular', {
+        .when(Routes.Popular, {
             templateUrl: 'sections/popular/popular.tpl.html',
             controller: 'PopularController as popular',
             resolve: {
-                shows: function(ShowService) {
+                shows: ['ShowService', (ShowService: IShowService) => {
                     return ShowService.getPopular();
-                }
+                }]
             }
         })
-        .when('/view/:id', {
+        .when(Routes.View, {
             templateUrl: 'sections/view/view.tpl.html',
             controller: 'ViewController as view',
             resolve: {
-                show: function(ShowService, $route) {
+                show: ['ShowService', '$route', (ShowService: IShowService, $route: any) => {
                     return ShowService.get($route.current.params.id);
-                }
+                }]
             }
         })
         .otherwise({
-            redirectTo: '/'
+            redirectTo: Routes.Home
         });
 }
