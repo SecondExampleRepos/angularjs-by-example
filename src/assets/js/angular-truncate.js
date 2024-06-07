12603517b993a1289be497fe1e98d1c068538de7
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-07 13:18:05 +0000

    edit_file

--- src/assets/js/angular-truncate.js
+++ src/assets/js/angular-truncate.js
@@ -1,20 +1,25 @@
+import * as angular from 'angular';
+type TruncateFilter = (input: string, limit: number, breakOnWord?: boolean) => string;
+enum FilterType {
+    Characters = 'characters',
+    Words = 'words'
+}
 angular.module('truncate', [])
-.filter('characters', function () {
-    return function (input, chars, breakOnWord) {
+.filter(FilterType.Characters, function (): TruncateFilter {
+    return function (input: string, chars: number, breakOnWord: boolean = false): string {
         if (isNaN(chars)) return input;
         if (chars <= 0) return '';
         if (input && input.length > chars) {
             input = input.substring(0, chars);
-
             if (!breakOnWord) {
-                var lastspace = input.lastIndexOf(' ');
+                const lastspace = input.lastIndexOf(' ');
                 //get last space
                 if (lastspace !== -1) {
                     input = input.substr(0, lastspace);
                 }
-            }else{
-                while(input.charAt(input.length-1) === ' '){
-                    input = input.substr(0, input.length -1);
+            } else {
+                while (input.charAt(input.length - 1) === ' ') {
+                    input = input.substr(0, input.length - 1);
                 }
             }
             return input + '...';
@@ -22,12 +27,12 @@ angular.module('truncate', [])
         return input;
     };
 })
-.filter('words', function () {
-    return function (input, words) {
+.filter(FilterType.Words, function (): TruncateFilter {
+    return function (input: string, words: number): string {
         if (isNaN(words)) return input;
         if (words <= 0) return '';
         if (input) {
-            var inputWords = input.split(/\s+/);
+            const inputWords = input.split(/\s+/);
             if (inputWords.length > words) {
                 input = inputWords.slice(0, words).join(' ') + '...';
             }
