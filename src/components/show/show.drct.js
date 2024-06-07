b5321e85b1b80b11c503f5caf233afb53a262904
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-07 13:17:56 +0000

    edit_file

--- src/components/show/show.drct.js
+++ src/components/show/show.drct.js
@@ -1,20 +1,26 @@
-angular
-    .module('app.core')
-    .directive('show', show);
-function show(ShowService) {
-    var directive = {
-        controller: controller,
-        templateUrl: 'components/show/show.tpl.html',
-        restrict: 'E',
-        scope: {
-            show: '='
-        }
-    };
-    return directive;
-    function controller($scope) {
-        $scope.genres = [];
-        ShowService.get($scope.show.id).then(function(response){
-            $scope.genres = response.genres;
+import { IComponentOptions, IController } from 'angular';
+import { ShowService } from './show.service';
+interface IShowScope extends ng.IScope {
+    show: { id: number };
+    genres: string[];
+}
+class ShowController implements IController {
+    static $inject = ['$scope', 'ShowService'];
+    constructor(private $scope: IShowScope, private ShowService: ShowService) {
+        this.$scope.genres = [];
+        this.ShowService.get(this.$scope.show.id).then((response: { genres: string[] }) => {
+            this.$scope.genres = response.genres;
         });
     }
 }
+const showDirective: IComponentOptions = {
+    controller: ShowController,
+    templateUrl: 'components/show/show.tpl.html',
+    restrict: 'E',
+    scope: {
+        show: '='
+    }
+};
+angular
+    .module('app.core')
+    .component('show', showDirective);
