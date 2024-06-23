706c102451c68c0e7bfb5ffa03a238e3beb95f92
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-23 12:34:37 +0000

    create_file_with_ai
    # Path: step_7/for_each/for_each/create_file_with_ai
    # UUID: 4623d651-ff58-4d61-a84c-a269af42c0d7
    
    # Changed files:
    react/src/components/ngAnimateChildren.tsx

---
+++ react/src/components/ngAnimateChildren.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+interface NgAnimateChildrenProps {
+  animateChildren: boolean | string;
+  children: React.ReactNode;
+}
+
+const NgAnimateChildren: React.FC<NgAnimateChildrenProps> = ({ animateChildren, children }) => {
+  const [shouldAnimateChildren, setShouldAnimateChildren] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (typeof animateChildren === 'string' && animateChildren.length === 0) {
+      setShouldAnimateChildren(true);
+    } else {
+      setShouldAnimateChildren(!!animateChildren);
+    }
+  }, [animateChildren]);
+
+  return (
+    <div data-animate-children={shouldAnimateChildren}>
+      {children}
+    </div>
+  );
+};
+
+export default NgAnimateChildren;
