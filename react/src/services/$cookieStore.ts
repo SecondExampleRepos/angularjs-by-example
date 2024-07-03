1ecc6485429df97cecc9acbf058383ab96abd0d5
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-07-03 04:19:16 -0700

    create_file_with_ai
    # Path: [Step 5]/for_each/for_each/create_file_with_ai
    # UUID: 7ea54bdf-2d62-4654-a965-b520988fde8e
    
    # Changed files:
    react/src/services/$cookieStore.ts

---
+++ react/src/services/$cookieStore.ts
@@ -0,0 +1,41 @@
+// react/src/services/$cookieStore.ts
+
+import { fromJson, toJson } from '../utils/jsonUtils';
+
+interface Cookies {
+  [key: string]: string;
+}
+
+class CookieStore {
+  private cookies: Cookies;
+
+  constructor() {
+    this.cookies = this.getCookies();
+  }
+
+  private getCookies(): Cookies {
+    const cookies: Cookies = {};
+    document.cookie.split(';').forEach(cookie => {
+      const [name, value] = cookie.split('=');
+      cookies[name.trim()] = value;
+    });
+    return cookies;
+  }
+
+  public get(key: string): any {
+    const value = this.cookies[key];
+    return value ? fromJson(value) : value;
+  }
+
+  public put(key: string, value: any): void {
+    this.cookies[key] = toJson(value);
+    document.cookie = `${key}=${this.cookies[key]}`;
+  }
+
+  public remove(key: string): void {
+    delete this.cookies[key];
+    document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+  }
+}
+
+export const cookieStore = new CookieStore();
