795bb4eb157ab8a2a1cf9bad94cb8ae0eb1be4e2
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-07-03 04:50:17 -0700

    create_file_with_ai
    # Path: [Step 6]/create_file_with_ai
    # UUID: 87236a37-9a12-410c-b276-2b03e1668c2d
    
    # Changed files:
    react/src/routes/RootRouter.tsx

---
+++ react/src/routes/RootRouter.tsx
@@ -0,0 +1,26 @@
+// react/src/routes/RootRouter.tsx
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
+// Import and use all the routes in this file, which will be the root router file imported into the main App.tsx file.
+const RootRouter: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+
+        <Route path="/services" component={Services} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/blog" component={Blog} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default RootRouter;
