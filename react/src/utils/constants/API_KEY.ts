8b05bf30c852de6cac67d83d63a9e07fa41fdf5e
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-07-16 18:39:38 -0700

    edit_file
    # Path: [2. Convert files in order]/for_each/for_each/switch/case_2/for_each/edit_file
    # UUID: cd556ee5-5929-45e3-a273-538c0c40096b
    
    # Changed files:
    react/src/utils/constants/API_KEY.ts

---
+++ react/src/utils/constants/API_KEY.ts
@@ -0,0 +1,8 @@
+// react/src/utils/constants/API_KEY.ts
+
+/**
+ * Contains the API key for communicating with the TRACK TV API
+ */
+const API_KEY: string = '87de9079e74c828116acce677f6f255b';
+
+export default API_KEY;
