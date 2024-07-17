14a97686dd963ff9af4d5ed00d245a35e30fb0da
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-07-16 18:32:47 -0700

    edit_file
    # Path: [2. Convert files in order]/for_each/for_each/switch/case_2/for_each/edit_file
    # UUID: a31283f3-3285-419a-af62-7392587390e4
    
    # Changed files:
    react/src/utils/constants/BASE_URL.ts

---
+++ react/src/utils/constants/BASE_URL.ts
@@ -0,0 +1,8 @@
+// react/src/utils/constants/BASE_URL.ts
+
+/**
+ * Contains the base URL for the TRACK TV API
+ */
+const BASE_URL: string = 'http://api.themoviedb.org/3';
+
+export default BASE_URL;
