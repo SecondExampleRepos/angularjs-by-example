0811b6c895415408696e95001b4f8e49d67b111c
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-07-25 17:32:05 +0000

    edit_file
    # Path: [4. Convert files in order]/for_each/for_each/if_else/if/switch/case_7/for_each/edit_file
    # UUID: 15fec8c2-3ca5-48f6-8260-097cdc07ed29
    
    # Changed files:
    react/src/components/ViewController.tsx

---
+++ react/src/components/ViewController.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import PageValues from '../utils/constants/PageValues';
+import { API_KEY } from '../utils/constants/API_KEY';
+import BASE_URL from '../utils/constants/BASE_URL';
+
+interface Show {
+    id: number;
+    original_name: string;
+    cast: Array<any>;
+}
+
+interface ViewControllerProps {
+    show: Show;
+}
+
+const ViewController: React.FC<ViewControllerProps> = ({ show }) => {
+    const [cast, setCast] = useState<Array<any>>([]);
+
+    useEffect(() => {
+        // Set page title and description
+        PageValues.title = "VIEW";
+        PageValues.description = `Overview, seasons & info for '${show.original_name}'.`;
+
+        // Fetch cast information
+        const fetchCast = async () => {
+            try {
+                const response = await axios.get(`${BASE_URL}/tv/${show.id}/credits?api_key=${API_KEY}`);
+                setCast(response.data.cast);
+            } catch (error) {
+                console.error('Error fetching cast information:', error);
+            }
+        };
+
+        fetchCast();
+    }, [show]);
+
+    const setBannerImage = () => {
+        return {
+            background: 'url() no-repeat',
+            backgroundSize: '100%',
+            backgroundPosition: '100% 0%'
+        };
+    };
+
+    return (
+        <div>
+            <h1>{PageValues.title}</h1>
+            <p>{PageValues.description}</p>
+            <div style={setBannerImage()}></div>
+            <ul>
+                {cast.map((member, index) => (
+                    <li key={index}>{member.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ViewController;
