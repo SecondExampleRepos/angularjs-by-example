1887e221227dfa63bd6bf41a6f47dd570d789cbc
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-07-30 09:33:00 -0700

    edit_file
    # Path: [3. Convert files in order]/for_each/for_each/if_else/if/switch/case_5/for_each/edit_file
    # UUID: 143243b9-0f2e-458e-aaba-d03e41ae9791
    
    # Changed files:
    react/src/routes/config.ts
    - a972ecdadc7b9ddf56ed1040b78ca49ecabfb48a

---
+++ react/src/routes/config.ts
@@ -0,0 +1,43 @@
+﻿import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomeController from '../components/HomeController';
+import PremieresController from '../components/PremieresController';
+import SearchController from '../components/SearchController';
+import PopularController from '../components/PopularController';
+import ViewController from '../components/ViewController';
+import ShowService from '../services/ShowService';
+
+const Routes = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={HomeController} />
+                <Route 
+                    path="/premieres" 
+                    render={async (props) => {
+                        const shows = await ShowService.getPremieres();
+                        return <PremieresController {...props} shows={shows} />;
+                    }} 
+                />
+                <Route exact path="/search" component={SearchController} />
+                <Route path="/search/:searchQuery" component={SearchController} />
+                <Route 
+                    path="/popular" 
+                    render={async (props) => {
+                        const shows = await ShowService.getPopular();
+                        return <PopularController {...props} shows={shows} />;
+                    }} 
+                />
+                <Route 
+                    path="/view/:id" 
+                    render={async (props) => {
+                        const show = await ShowService.get(props.match.params.id);
+                        return <ViewController {...props} show={show} />;
+                    }} 
+                />
+                <Route path="*" component={HomeController} />
+            </Switch>
+        </Router>
+    );
+};
+
+export default Routes;
