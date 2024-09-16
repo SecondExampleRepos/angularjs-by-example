33f7553bec43409f9da1b473d6dc334a5cac999f
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-16 09:42:15 -0700

    edit_file
    # Path: [6. Convert files in order]/for_each/async_each/if_else/if/switch/case_5/async_each/edit_file
    # UUID: 5987e411-9403-4c34-8316-51f69412a3d2
    
    # Changed files:
    react/src/routes/configs.tsx
    - dce5aeea64ff36eea917bd41d1fbc39bcb048d91

---
+++ react/src/routes/configs.tsx
@@ -0,0 +1,34 @@
+﻿// Converted from src/app.config.js
+
+import axios, { AxiosResponse } from 'axios';
+import { useLocation } from 'react-router-dom';
+
+export const useHttpInterceptor = () => {
+  const location = useLocation();
+
+  const errorInterceptor = (error: any) => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 401:
+          console.error('You are unauthorised to access the requested resource (401)');
+          break;
+        case 404:
+          console.error('The requested resource could not be found (404)');
+          break;
+        case 500:
+          console.error('Internal server error (500)');
+          break;
+        default:
+          console.error('An unknown error occurred');
+      }
+    }
+    return Promise.reject(error);
+  };
+
+  const successInterceptor = (response: AxiosResponse) => {
+    // Request completed successfully
+    return response;
+  };
+
+  axios.interceptors.response.use(successInterceptor, errorInterceptor);
+};
