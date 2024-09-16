9038997b45cee3306acfbd398dcd9cdf080ca3c6
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-16 09:43:18 -0700

    edit_file
    # Path: [6. Convert files in order]/for_each/async_each/if_else/if/switch/case_2/async_each/edit_file
    # UUID: 32f9c57a-724a-460c-ad30-48ee57d2fc00
    
    # Changed files:
    react/src/utils/constants/BASE_URL.ts

---
+++ react/src/utils/constants/BASE_URL.ts
@@ -0,0 +1,6 @@
+﻿// Converted from src/services/show.fct.js
+
+// Define the BASE_URL constant for use in React components
+const BASE_URL: string = 'http://api.themoviedb.org/3';
+
+export default BASE_URL;
