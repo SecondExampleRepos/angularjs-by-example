35090ce783dbc8068c39b1b2473dc350c0f049ed
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-16 09:43:17 -0700

    edit_file
    # Path: [6. Convert files in order]/for_each/async_each/if_else/if/switch/case_2/async_each/edit_file
    # UUID: 03f4e5fc-4ac2-4a4a-a429-4a944ccc1b98
    
    # Changed files:
    react/src/utils/constants/API_KEY.ts

---
+++ react/src/utils/constants/API_KEY.ts
@@ -0,0 +1,9 @@
+﻿// Converted from src/services/show.fct.js
+
+// Define the API_KEY type based on the AngularJS constant
+type ApiKeyType = string;
+
+// Export the API_KEY constant for use in React components
+const API_KEY: ApiKeyType = '87de9079e74c828116acce677f6f255b';
+
+export default API_KEY;
