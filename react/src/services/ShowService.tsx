c030a29f65a3af82baabb536746fe14cdf43340b
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-17 10:02:10 +0000

    edit_file
    # Path: [6. Convert files in order]/for_each/async_each/if_else/if/switch/case_4/async_each/edit_file
    # UUID: 4f870d8c-687f-4c64-a527-6ab8a370093c
    
    # Changed files:
    react/src/services/ShowService.tsx

---
+++ react/src/services/ShowService.tsx
@@ -0,0 +1,69 @@
+﻿// Converted from src/services/show.fct.js
+
+import axios from 'axios';
+import moment from 'moment';
+
+const API_KEY = '87de9079e74c828116acce677f6f255b';
+const BASE_URL = 'http://api.themoviedb.org/3';
+
+interface ShowService {
+  getPremieres: () => Promise<any>;
+  get: (id: string) => Promise<any>;
+  search: (query: string) => Promise<any>;
+  getPopular: () => Promise<any>;
+  getCast: (id: string) => Promise<any>;
+}
+
+const makeRequest = async (url: string, params: Record<string, any>) => {
+  const requestUrl = `${BASE_URL}/${url}?api_key=${API_KEY}`;
+  const queryString = Object.entries(params)
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&');
+  
+  try {
+    const response = await axios.get(`${requestUrl}&${queryString}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('XHR Failed for ShowService', error);
+    throw error;
+  }
+};
+
+const showService: ShowService = {
+  getPremieres: async () => {
+    const date = new Date();
+    date.setDate(1);
+    const formattedDate = moment(date).format('DD-MM-YYYY');
+    
+    const data = await makeRequest('discover/tv', { 
+      'first_air_date.gte': formattedDate, 
+      append_to_response: 'genres' 
+    });
+    
+    return data.results;
+  },
+
+  get: async (id) => {
+    return makeRequest(`tv/${id}`, {});
+  },
+
+  search: async (query) => {
+    const data = await makeRequest('search/tv', { query });
+    return data.results;
+  },
+
+  getPopular: async () => {
+    const data = await makeRequest('tv/popular', {});
+    return data.results;
+  },
+
+  getCast: async (id) => {
+    return makeRequest(`tv/${id}/credits`, {});
+  },
+};
+
+export default showService;
