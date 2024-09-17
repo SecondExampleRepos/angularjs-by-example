dd52634290e7f8c21a419f06416f4156ac62069e
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-17 15:19:55 -0700

    edit_file
    # Path: [6. Convert files in order]/for_each/async_each/if_else/if/switch/case_5/async_each/edit_file
    # UUID: da63bb25-3fe5-42a7-b0e3-cce424a919e4
    
    # Changed files:
    react/src/routes/config.tsx
    - 19e40875ada54f10ed95c1ccc106894bb1601b57

---
+++ react/src/routes/config.tsx
@@ -0,0 +1,48 @@
+﻿// Converted from src/app.routes.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Home from '../components/home';
+import Premieres from '../components/premieres';
+import Search from '../components/search';
+import Popular from '../components/popular';
+import View from '../components/view';
+import ShowService from '../services/showService';
+
+const AppRoutes: React.FC = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route 
+                    path="/premieres" 
+                    render={() => (
+                        <Premieres shows={ShowService.getPremieres()} />
+                    )}
+                />
+                <Route exact path="/search" component={Search} />
+                <Route 
+                    path="/search/:query" 
+                    render={({ match }) => (
+                        <Search query={match.params.query} />
+                    )}
+                />
+                <Route 
+                    path="/popular" 
+                    render={() => (
+                        <Popular shows={ShowService.getPopular()} />
+                    )}
+                />
+                <Route 
+                    path="/view/:id" 
+                    render={({ match }) => (
+                        <View show={ShowService.get(match.params.id)} />
+                    )}
+                />
+                <Redirect to="/" />
+            </Switch>
+        </Router>
+    );
+};
+
+export default AppRoutes;
