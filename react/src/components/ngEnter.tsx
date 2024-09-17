0ed3c29fe084eecf0adc4fe2c5f18acd49d1863e
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-17 10:01:56 +0000

    edit_file
    # Path: [6. Convert files in order]/for_each/async_each/if_else/if/switch/case_6/async_each/edit_file
    # UUID: 8bee5aa7-adb6-4b20-9105-157672a9388f
    
    # Changed files:
    react/src/components/ngEnter.tsx

---
+++ react/src/components/ngEnter.tsx
@@ -0,0 +1,24 @@
+﻿// Converted from src/directives/ngEnter.drct.js
+
+import React, { useCallback } from 'react';
+
+interface NgEnterProps {
+    onEnter: () => void;
+}
+
+const NgEnter: React.FC<NgEnterProps> = ({ onEnter, children }) => {
+    const handleKeyPress = useCallback((event: React.KeyboardEvent) => {
+        if (event.key === 'Enter') {
+            onEnter();
+            event.preventDefault();
+        }
+    }, [onEnter]);
+
+    return (
+        <div onKeyPress={handleKeyPress}>
+            {children}
+        </div>
+    );
+};
+
+export default NgEnter;
