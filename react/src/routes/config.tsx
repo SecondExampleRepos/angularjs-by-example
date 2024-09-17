17e5c58221c3e2bed8857641aba342364b6cdf0a
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-17 11:22:18 +0000

    edit_file
    # Path: [6. Convert files in order]/for_each/async_each/if_else/if/switch/case_5/async_each/edit_file
    # UUID: 3c20888e-5317-4d55-b469-50442e7e342a
    
    # Changed files:
    react/src/routes/config.tsx
    - d0ce684d42a706474098c01444f1069d324f3820

---
+++ react/src/routes/config.tsx
@@ -0,0 +1,46 @@
+﻿// Converted from src/app.routes.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import HomeController from '../components/HomeController';
+import PremieresController from '../components/PremieresController';
+import SearchController from '../components/SearchController';
+import PopularController from '../components/PopularController';
+import ViewController from '../components/ViewController';
+import ShowService from '../services/ShowService';
+
+const AppRoutes: React.FC = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={HomeController} />
+                <Route
+                    path="/premieres"
+                    render={async (props) => {
+                        const shows = await ShowService.getPremieres();
+                        return <PremieresController {...props} shows={shows} />;
+                    }}
+                />
+                <Route exact path="/search" component={SearchController} />
+                <Route path="/search/:query" component={SearchController} />
+                <Route
+                    path="/popular"
+                    render={async (props) => {
+                        const shows = await ShowService.getPopular();
+                        return <PopularController {...props} shows={shows} />;
+                    }}
+                />
+                <Route
+                    path="/view/:id"
+                    render={async (props) => {
+                        const show = await ShowService.get(props.match.params.id);
+                        return <ViewController {...props} show={show} />;
+                    }}
+                />
+                <Redirect to="/" />
+            </Switch>
+        </Router>
+    );
+};
+
+export default AppRoutes;
