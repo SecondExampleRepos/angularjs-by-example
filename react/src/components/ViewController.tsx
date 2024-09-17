4c397b09f6018cf28421e9ae76f8157b1ef0679c
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-17 16:01:43 -0700

    edit_file
    # Path: [6. Convert files in order]/for_each/async_each/if_else/if/switch/case_7/async_each/edit_file
    # UUID: 9b71459f-9bae-4cfd-94cb-fd5e6bcd026a
    
    # Changed files:
    react/src/components/ViewController.tsx

---
+++ react/src/components/ViewController.tsx
@@ -0,0 +1,57 @@
+﻿// Converted from src/sections/view/view.ctrl.js
+
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import PageValues from '../../utils/constants/PageValues';
+
+type ShowType = {
+    id: number;
+    original_name: string;
+    cast?: Array<any>;
+};
+
+type CastType = {
+    cast: Array<any>;
+};
+
+const ViewController: React.FC<{ show: ShowType }> = ({ show }) => {
+    const [cast, setCast] = useState<Array<any>>([]);
+    const { id } = useParams<{ id: string }>();
+
+    useEffect(() => {
+        PageValues.title = "VIEW";
+        PageValues.description = `Overview, seasons & info for '${show.original_name}'.`;
+
+        const fetchCast = async () => {
+            try {
+                const response = await axios.get<CastType>(`/tv/${id}/credits`);
+                setCast(response.data.cast);
+            } catch (error) {
+                console.error('Failed to fetch cast', error);
+            }
+        };
+
+        fetchCast();
+    }, [id, show.original_name]);
+
+    const setBannerImage = () => ({
+        background: 'url() no-repeat',
+        backgroundSize: '100%',
+        backgroundPosition: '100% 0%',
+    });
+
+    return (
+        <div style={setBannerImage()}>
+            {/* Render the show and cast information here */}
+            <h1>{show.original_name}</h1>
+            <ul>
+                {cast.map((member, index) => (
+                    <li key={index}>{member.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ViewController;
