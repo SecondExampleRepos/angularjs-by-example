cf9b43d3dbbc5d4a4f5a26b9a30ddeaa024659cf
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-18 09:23:53 -0700

    edit_file
    # Path: [7. Combine Routes into a single file and add to App.tsx]/edit_file
    # UUID: c529c1c8-b4aa-4dab-a40e-a5de388d950d
    
    # Changed files:
    react/src/routes/Router.tsx
    - bd096316141de8503e9458241137cea80268a560

--- react/src/routes/Router.tsx
+++ react/src/routes/Router.tsx
@@ -1,11 +1,21 @@
 ﻿import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from '../components/controllers/HomeController';
+import Premieres from '../components/controllers/PremieresController';
+import Search from '../components/controllers/SearchController';
+import Popular from '../components/controllers/PopularController';
+import View from '../components/controllers/ViewController';
 
 const AppRouter = () => {
     return (
         <Router>
             <Routes>
-                {/* Add routes here */}
+                <Route path="/" element={<Home />} />
+                <Route path="/premieres" element={<Premieres />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/search/:query" element={<Search />} />
+                <Route path="/popular" element={<Popular />} />
+                <Route path="/view/:id" element={<View />} />
             </Routes>
         </Router>
     );
