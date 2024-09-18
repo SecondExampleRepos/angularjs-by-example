095e1665e2f94a83505b643d0b6dc7a9686ed7db
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-18 15:13:22 -0700

    edit_file
    # Path: [7. Add CSS imports to App.tsx]/edit_file
    # UUID: e1455515-9ff2-4c7d-9973-3369d50c4182
    
    # Changed files:
    react/src/App.tsx
    - d61bc0bf2d489b96da673719eb880e6600dfbf23
    - d61bc0bf2d489b96da673719eb880e6600dfbf23
    - 62d0735941c1f1314c1a35d447e19ec46d256b6d
    - 4f6e14c8d380cc9a2abaca02217a173359cb42c8

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -6,6 +6,18 @@ import Search from '../../components/controllers/Search';
 import Popular from '../../components/controllers/Popular';
 import View from '../../components/controllers/View';
 
+// CSS imports
+import 'react/src/App.css';
+import 'react/src/index.css';
+import 'src/sections/home/home.css';
+import 'src/sections/premieres/premieres.css';
+import 'src/sections/search/search.css';
+import 'src/sections/view/view.css';
+import 'src/components/show/show.css';
+import 'src/assets/css/animations.css';
+import 'src/assets/css/font-icons.css';
+import 'src/assets/css/style.css';
+
 // Converted from src/app.routes.js
 
 function App() {
