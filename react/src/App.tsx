e9991ff1e9286dda4bfb4095f7b28b5bec835529
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-18 13:32:53 -0700

    validate_typescript_project
    # Path: [7. Validate TypeScript Project]/validate_typescript_project
    # UUID: dd3de055-1a12-4991-9809-a981485278d6
    
    # Changed files:
    react/src/App.tsx
    - ff89fd95f63dfdcc3b508ca1714e6ce14da7748b
    - ff89fd95f63dfdcc3b508ca1714e6ce14da7748b
    - e1521fcc3ec80e8aeca1dda8ea8f14a92c80df61
    - 5e19a7ee9128166c795266f6597e6edc30ebefc7
    react/src/components/controllers/SearchController.tsx
    - c9dec90ab440f8e04eca8aca57d4e9b1d623f6f3
    react/src/services/ShowService.tsx
    - 4e3bdd5ff71e29a103f190f1edf33944eae2ec98

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,11 +2,11 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Home from '../../components/controllers/HomeController';
-import Premieres from '../../components/controllers/PremieresController';
-import Search from '../../components/controllers/SearchController';
-import Popular from '../../components/controllers/PopularController';
-import View from '../../components/controllers/ViewController';
+import Home from './components/controllers/HomeController';
+import Premieres from './components/controllers/PremieresController';
+import Search from './components/controllers/SearchController';
+import Popular from './components/controllers/PopularController';
+import View from './components/controllers/ViewController';
 
 function App() {
   return (
