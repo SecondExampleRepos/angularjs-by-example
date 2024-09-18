b0c3dd08ce15b2c767f95e515bfaf37241570d6e
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-18 09:01:00 -0700

    edit_file
    # Path: [6. Convert files in order]/for_each/async_each/if_else/if/switch/case_5/async_each/edit_file
    # UUID: a388ca41-a020-4c7d-ad7f-d6054a105445
    
    # Changed files:
    react/src/routes/config.tsx
    - 3d2eae2ad695b424266091ca3c221dfb33454555

---
+++ react/src/routes/config.tsx
@@ -0,0 +1,21 @@
+﻿// Converted from src/app.routes.js
+
+import { Route, Routes } from 'react-router-dom';
+import Home from '../components/controllers/HomeController';
+import Premieres from '../components/controllers/PremieresController';
+import Search from '../components/controllers/SearchController';
+import Popular from '../components/controllers/PopularController';
+import View from '../components/controllers/ViewController';
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/premieres" element={<Premieres />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/search/:query" element={<Search />} />
+      <Route path="/popular" element={<Popular />} />
+      <Route path="/view/:id" element={<View />} />
+    </Routes>
+  );
+}
