3b840a5335e181443e60df471c1c7759ea8ea422
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-18 11:43:01 -0700

    edit_file
    # Path: [5. Convert files in order]/for_each/async_each/if_else/if/switch/case_7/async_each/edit_file
    # UUID: af838b5d-98b4-4786-8312-54e8be60cf1d
    
    # Changed files:
    react/src/components/controllers/PremieresController.tsx

---
+++ react/src/components/controllers/PremieresController.tsx
@@ -0,0 +1,37 @@
+﻿// Converted from src/sections/premieres/premieres.ctrl.js
+
+import React, { useEffect, useState } from 'react';
+import ShowService from '../../services/showService';
+import PageValues from '../../utils/constants/PageValues';
+
+type Show = {
+    id: number;
+    name: string;
+};
+
+const PremieresController: React.FC = () => {
+    const [shows, setShows] = useState<Show[]>([]);
+
+    useEffect(() => {
+        PageValues.title = "PREMIERES";
+        PageValues.description = "Brand new shows showing this month.";
+
+        ShowService.getPremieres().then((data) => {
+            setShows(data);
+        });
+    }, []);
+
+    return (
+        <div>
+            <h1>{PageValues.title}</h1>
+            <p>{PageValues.description}</p>
+            <ul>
+                {shows.map((show, index) => (
+                    <li key={index}>{show.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default PremieresController;
