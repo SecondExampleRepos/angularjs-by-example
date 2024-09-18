135b34082130a64091a594e9904d96e253807e07
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-17 17:13:53 -0700

    edit_file
    # Path: [6. Convert files in order]/for_each/async_each/if_else/if/switch/case_5/async_each/edit_file
    # UUID: 7b811240-d9b9-4991-a4d4-c055c7830d77
    
    # Changed files:
    react/src/routes/config.tsx
    - ef389ac44373da6159cfc675eae1755d8e5a7871

---
+++ react/src/routes/config.tsx
@@ -0,0 +1,47 @@
+﻿// Converted from src/app.routes.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Home from '../components/home';
+import Premieres from '../components/premieres';
+import Search from '../components/search';
+import Popular from '../components/popular';
+import View from '../components/view';
+
+const AppRoutes: React.FC = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route 
+                    path="/premieres" 
+                    render={(props) => (
+                        <Premieres {...props} />
+                    )}
+                />
+                <Route exact path="/search" component={Search} />
+                <Route 
+                    path="/search/:query" 
+                    render={(props) => (
+                        <Search {...props} />
+                    )}
+                />
+                <Route 
+                    path="/popular" 
+                    render={(props) => (
+                        <Popular {...props} />
+                    )}
+                />
+                <Route 
+                    path="/view/:id" 
+                    render={(props) => (
+                        <View {...props} />
+                    )}
+                />
+                <Redirect to="/" />
+            </Switch>
+        </Router>
+    );
+};
+
+export default AppRoutes;
