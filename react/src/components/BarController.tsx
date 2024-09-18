5e0dd4e53947d53e91e243add4ff817f91ced601
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-17 17:14:39 -0700

    edit_file
    # Path: [6. Convert files in order]/for_each/async_each/if_else/if/switch/case_7/async_each/edit_file
    # UUID: 83d54567-562e-43c9-bfed-121903704e14
    
    # Changed files:
    react/src/components/BarController.tsx

---
+++ react/src/components/BarController.tsx
@@ -0,0 +1,18 @@
+﻿// Converted from src/components/bar/bar.ctrl.js
+
+import React from 'react';
+import PageValues from '../utils/constants/PageValues';
+
+const BarController: React.FC = () => {
+    // Setup the view model object
+    const data = PageValues;
+
+    return (
+        <div>
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+        </div>
+    );
+};
+
+export default BarController;
