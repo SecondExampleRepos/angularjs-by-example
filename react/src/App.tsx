eeccf9aeea03b42b43471ec858aab333fca6e9a5
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-18 19:07:14 -0700

    validate_typescript_project
    # Path: [9. Validate TypeScript Project]/validate_typescript_project
    # UUID: 0be8a1a9-1cd9-44ed-bb75-51381111cf28
    
    # Changed files:
    react/src/App.tsx
    - 916f463420d5c58635642aab4ed250266590c5b2
    - 916f463420d5c58635642aab4ed250266590c5b2
    - 1f84949946a8dcd16dcf75196bc02c54ad3e8fbb
    - 0aadf2b98d643174004c25c6ba044b881e2d9639
    - 7793923b8391857aa8b97bef611bb10b6f86e1e2
    react/src/components/controllers/SearchController.tsx
    - 23e925a06d7c969b86015417dc6626ee94abc6a8
    react/src/components/directives/ngEnter.tsx
    - 5fcff5e26091127e77a60e745e9cb31c1c9c40f3
    react/src/services/ShowService.tsx
    - bfaec70eeda6394378e9753c54ca673a044b4542

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,23 +2,23 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Home from '../../components/controllers/HomeController';
-import Premieres from '../../components/controllers/PremieresController';
-import Search from '../../components/controllers/SearchController';
-import Popular from '../../components/controllers/PopularController';
-import View from '../../components/controllers/ViewController';
+import Home from './components/controllers/HomeController';
+import Premieres from './components/controllers/PremieresController';
+import Search from './components/controllers/SearchController';
+import Popular from './components/controllers/PopularController';
+import View from './components/controllers/ViewController';
 
 // CSS imports
-import 'react/src/assets/show.css';
-import 'react/src/assets/home.css';
-import 'react/src/assets/animations.css';
-import 'react/src/assets/search.css';
-import 'react/src/assets/App.css';
-import 'react/src/assets/index.css';
-import 'react/src/assets/font-icons.css';
-import 'react/src/assets/premieres.css';
-import 'react/src/assets/style.css';
-import 'react/src/assets/view.css';
+import './assets/show.css';
+import './assets/home.css';
+import './assets/animations.css';
+import './assets/search.css';
+import './assets/App.css';
+import './assets/index.css';
+import './assets/font-icons.css';
+import './assets/premieres.css';
+import './assets/style.css';
+import './assets/view.css';
 
 function App() {
   return (
