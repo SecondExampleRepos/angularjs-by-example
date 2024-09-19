5b6f3c8f4d7c59aacf39667cb001e7b8707388f9
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-19 16:33:37 -0700

    edit_file
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_5/async_each/edit_file
    # UUID: 19e2cdb1-7b38-4c11-bfa0-a85259b03383
    
    # Changed files:
    react/src/utils/config/configs.tsx

---
+++ react/src/utils/config/configs.tsx
@@ -0,0 +1,41 @@
+﻿// Converted from src/app.config.js
+
+import axios, { AxiosResponse } from 'axios';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+export function useConfigs() {
+    const location = useLocation();
+
+    useEffect(() => {
+        const interceptor = axios.interceptors.response.use(
+            (response: AxiosResponse) => {
+                // Request completed successfully
+                return response;
+            },
+            (error) => {
+                if (error.response) {
+                    switch (error.response.status) {
+                        case 401:
+                            toast.error('You are unauthorised to access the requested resource (401)');
+                            break;
+                        case 404:
+                            toast.error('The requested resource could not be found (404)');
+                            break;
+                        case 500:
+                            toast.error('Internal server error (500)');
+                            break;
+                        default:
+                            toast.error('An unexpected error occurred');
+                    }
+                }
+                return Promise.reject(error);
+            }
+        );
+
+        return () => {
+            axios.interceptors.response.eject(interceptor);
+        };
+    }, [location]);
+}
