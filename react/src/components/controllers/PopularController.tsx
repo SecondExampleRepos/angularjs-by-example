70889fd083b29d7df6aff36477df5b780b028f16
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-18 17:45:10 -0700

    edit_file
    # Path: [5. Convert files in order]/for_each/async_each/if_else/if/switch/case_7/async_each/edit_file
    # UUID: 78c68cdd-cc02-4e68-afd9-c1988ae264d3
    
    # Changed files:
    react/src/components/controllers/PopularController.tsx

---
+++ react/src/components/controllers/PopularController.tsx
@@ -0,0 +1,33 @@
+﻿// Converted from src/sections/popular/popular.ctrl.js
+
+import React, { useEffect, useState } from 'react';
+import ShowService from '../../services/ShowService';
+
+interface Show {
+  id: number;
+  original_name: string;
+}
+
+const PopularController: React.FC = () => {
+  const [shows, setShows] = useState<Show[]>([]);
+
+  useEffect(() => {
+    ShowService.getPopular().then((data) => {
+      setShows(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>POPULAR</h1>
+      <p>The most popular TV shows.</p>
+      <ul>
+        {shows.map((show) => (
+          <li key={show.id}>{show.original_name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PopularController;
