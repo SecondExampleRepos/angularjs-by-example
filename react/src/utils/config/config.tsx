2ce4b6645d54316e594f9dded3f52dc365f81b90
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-19 16:24:42 -0700

    edit_file
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_5/async_each/edit_file
    # UUID: 3c1fb9ee-fcb3-4aeb-8e87-1793f8e3d378
    
    # Changed files:
    react/src/utils/config/config.tsx
    - 559917714cd75f16cda6dc6ded927f569918b158

---
+++ react/src/utils/config/config.tsx
@@ -0,0 +1,45 @@
+﻿// Converted from src/app.routes.js
+
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Home from '../../components/controllers/Home';
+import Premieres from '../../components/controllers/Premieres';
+import Search from '../../components/controllers/Search';
+import Popular from '../../components/controllers/Popular';
+import View from '../../components/controllers/View';
+import ShowService from '../../services/ShowService';
+
+export function AppRoutes() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route
+                    path="/premieres"
+                    render={(props) => (
+                        <Premieres {...props} shows={ShowService.getPremieres()} />
+                    )}
+                />
+                <Route exact path="/search" component={Search} />
+                <Route
+                    path="/search/:query"
+                    render={(props) => (
+                        <Search {...props} query={props.match.params.query} />
+                    )}
+                />
+                <Route
+                    path="/popular"
+                    render={(props) => (
+                        <Popular {...props} shows={ShowService.getPopular()} />
+                    )}
+                />
+                <Route
+                    path="/view/:id"
+                    render={(props) => (
+                        <View {...props} show={ShowService.get(props.match.params.id)} />
+                    )}
+                />
+                <Redirect to="/" />
+            </Switch>
+        </Router>
+    );
+}
