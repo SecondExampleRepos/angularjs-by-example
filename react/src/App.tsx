6d387d9c35179a9b4d42528e0926ea1c8f0bb2d5
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-18 18:45:43 -0700

    validate_typescript_project
    # Path: [8. Validate TypeScript Project]/validate_typescript_project
    # UUID: 8be440c5-8816-4de0-8065-ca0496d6bea7
    
    # Changed files:
    react/src/App.tsx
    - 700fe1320990516ec4e3b0b043ad060154117b6c
    - 700fe1320990516ec4e3b0b043ad060154117b6c
    - b3ecbdaf70da41969c4cfa48166341034434c774
    - 48ee405732be6f324e0c496c884669ec4b828de1
    - 64a28df829ed7e63cbafb3f2944ac4e9d479c69e
    react/src/components/controllers/SearchController.tsx
    - 4206acb577aeddb21a24dbd09b792414604a0ffc
    react/src/components/directives/ngEnter.tsx
    - 3cdaebb211a799209e9d1da18dd18bbc4783854e
    react/src/services/ShowService.tsx
    - e1e28abdc1e3224ee64fa2a42ca51c18608a3e5c

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,29 +2,29 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Home from '../../components/controllers/HomeController';
-import Premieres from '../../components/controllers/PremieresController';
-import Search from '../../components/controllers/SearchController';
-import Popular from '../../components/controllers/PopularController';
-import View from '../../components/controllers/ViewController';
+import HomeController from './components/controllers/HomeController';
+import PremieresController from './components/controllers/PremieresController';
+import SearchController from './components/controllers/SearchController';
+import PopularController from './components/controllers/PopularController';
+import ViewController from './components/controllers/ViewController';
 
 // CSS imports
-import '../../assets/show.css';
-import '../../assets/home.css';
-import '../../assets/search.css';
-import '../../assets/premieres.css';
-import '../../assets/view.css';
+import '../assets/show.css';
+import '../assets/home.css';
+import '../assets/search.css';
+import '../assets/premieres.css';
+import '../assets/view.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/premieres" element={<Premieres />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/search/:query" element={<Search />} />
-        <Route path="/popular" element={<Popular />} />
-        <Route path="/view/:id" element={<View />} />
+        <Route path="/" element={<HomeController />} />
+        <Route path="/premieres" element={<PremieresController />} />
+        <Route path="/search" element={<SearchController />} />
+        <Route path="/search/:query" element={<SearchController />} />
+        <Route path="/popular" element={<PopularController />} />
+        <Route path="/view/:id" element={<ViewController />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
