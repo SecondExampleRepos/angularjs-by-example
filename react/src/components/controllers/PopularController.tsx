52c6c3fb3dae503740235e25c550da34c3714536
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-19 16:25:17 -0700

    edit_file
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_7/async_each/edit_file
    # UUID: 0dc88ff2-8778-4e3c-8caa-903c26c40e78
    
    # Changed files:
    react/src/components/controllers/PopularController.tsx

---
+++ react/src/components/controllers/PopularController.tsx
@@ -0,0 +1,36 @@
+﻿// Converted from src/sections/popular/popular.ctrl.js
+
+import React, { useEffect, useState } from 'react';
+import ShowService from '../../services/ShowService';
+import PageValues from '../../utils/constants/PageValues';
+
+type Show = {
+    id: number;
+    original_name: string;
+};
+
+const PopularController: React.FC = () => {
+    const [shows, setShows] = useState<Show[]>([]);
+
+    useEffect(() => {
+        PageValues.title = "POPULAR";
+        PageValues.description = "The most popular TV shows.";
+
+        ShowService.getPopular().then((data) => {
+            setShows(data);
+        });
+    }, []);
+
+    return (
+        <div>
+            <h1>Popular TV Shows</h1>
+            <ul>
+                {shows.map((show) => (
+                    <li key={show.id}>{show.original_name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default PopularController;
