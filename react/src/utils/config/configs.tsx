dfec5ac5f68bb954ba818f916f55e85588335baa
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-20 11:08:58 -0700

    edit_file
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_5/async_each/edit_file
    # UUID: 612bfa89-3173-4b5f-83ec-e2d9781deded
    
    # Changed files:
    react/src/utils/config/configs.tsx

---
+++ react/src/utils/config/configs.tsx
@@ -0,0 +1,21 @@
+﻿// Converted from src/app.config.js
+
+import axios, { AxiosResponse } from 'axios';
+
+export function configs() {
+    const interceptor = (response: AxiosResponse) => {
+        if (response.status === 401) {
+            console.error('You are unauthorised to access the requested resource (401)');
+        } else if (response.status === 404) {
+            console.error('The requested resource could not be found (404)');
+        } else if (response.status === 500) {
+            console.error('Internal server error (500)');
+        }
+        return Promise.reject(response);
+    };
+
+    axios.interceptors.response.use(
+        response => response, // Request completed successfully
+        interceptor
+    );
+}
