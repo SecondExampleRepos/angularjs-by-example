022d04d55b4746966c1640bb717f250926c379dd
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-20 11:09:07 -0700

    edit_file
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_5/async_each/edit_file
    # UUID: b0d454f0-47bb-4d00-9d73-e87dc7395a4d
    
    # Changed files:
    react/src/utils/config/config.tsx
    - dfec5ac5f68bb954ba818f916f55e85588335baa

---
+++ react/src/utils/config/config.tsx
@@ -0,0 +1,50 @@
+﻿// Converted from src/app.routes.js
+
+import { RouteObject } from 'react-router-dom';
+import Home from '../../components/controllers/Home';
+import Premieres from '../../components/controllers/Premieres';
+import Search from '../../components/controllers/Search';
+import Popular from '../../components/controllers/Popular';
+import View from '../../components/controllers/View';
+import ShowService from '../../services/ShowService';
+
+const routes: RouteObject[] = [
+    {
+        path: '/',
+        element: <Home />,
+    },
+    {
+        path: '/premieres',
+        element: <Premieres />,
+        loader: async () => {
+            const shows = await ShowService.getPremieres();
+            return { shows };
+        },
+    },
+    {
+        path: '/search',
+        element: <Search />,
+    },
+    {
+        path: '/search/:query',
+        element: <Search />,
+    },
+    {
+        path: '/popular',
+        element: <Popular />,
+        loader: async () => {
+            const shows = await ShowService.getPopular();
+            return { shows };
+        },
+    },
+    {
+        path: '/view/:id',
+        element: <View />,
+        loader: async ({ params }) => {
+            const show = await ShowService.get(params.id);
+            return { show };
+        },
+    },
+];
+
+export default routes;
