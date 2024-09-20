abbe89244281eb116a84d56827caf7c9d38cdf55
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-19 22:44:28 -0700

    edit_file
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_5/async_each/edit_file
    # UUID: 12ddac80-9800-497b-85fb-1540e18868b9
    
    # Changed files:
    react/src/utils/config/config.tsx
    - 4545d89d8ee8555f430116cc0dcfe39a64e2e38e

---
+++ react/src/utils/config/config.tsx
@@ -0,0 +1,48 @@
+﻿// Converted from src/app.routes.js
+
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import HomeController from '../../components/controllers/HomeController';
+import PremieresController from '../../components/controllers/PremieresController';
+import SearchController from '../../components/controllers/SearchController';
+import PopularController from '../../components/controllers/PopularController';
+import ViewController from '../../components/controllers/ViewController';
+
+export default function AppRoutes() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={HomeController} />
+                <Route 
+                    path="/premieres" 
+                    render={(props) => (
+                        <PremieresController {...props} />
+                    )}
+                />
+                <Route 
+                    exact 
+                    path="/search" 
+                    component={SearchController} 
+                />
+                <Route 
+                    path="/search/:query" 
+                    render={(props) => (
+                        <SearchController {...props} />
+                    )}
+                />
+                <Route 
+                    path="/popular" 
+                    render={(props) => (
+                        <PopularController {...props} />
+                    )}
+                />
+                <Route 
+                    path="/view/:id" 
+                    render={(props) => (
+                        <ViewController {...props} />
+                    )}
+                />
+                <Redirect to="/" />
+            </Switch>
+        </Router>
+    );
+}
