ace83bf5ad6194c6132635c0b36d64516be242bb
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-20 11:10:10 -0700

    edit_file
    # Path: [6. Combine Routes into a single file and add to App.tsx]/edit_file
    # UUID: be4d0c4d-5e38-4db3-a010-039a89612f15
    
    # Changed files:
    react/src/App.tsx
    - c961de082a3ada028f02264de44b97b67820375b
    - c961de082a3ada028f02264de44b97b67820375b
    - 47c7b74d5fc461a71873b81ea88863c6513d0959

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,10 +1,45 @@
 ﻿import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../../components/controllers/Home';
+import Premieres from '../../components/controllers/Premieres';
+import Search from '../../components/controllers/Search';
+import Popular from '../../components/controllers/Popular';
+import View from '../../components/controllers/View';
+import ShowService from '../../services/ShowService';
+
+// Converted from src/app.routes.js
 
 function App() {
   return (
-    <div>
-      {/* App content goes here */}
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/premieres"
+        element={<Premieres />}
+        loader={async () => {
+          const shows = await ShowService.getPremieres();
+          return { shows };
+        }}
+      />
+      <Route path="/search" element={<Search />} />
+      <Route path="/search/:query" element={<Search />} />
+      <Route
+        path="/popular"
+        element={<Popular />}
+        loader={async () => {
+          const shows = await ShowService.getPopular();
+          return { shows };
+        }}
+      />
+      <Route
+        path="/view/:id"
+        element={<View />}
+        loader={async ({ params }) => {
+          const show = await ShowService.get(params.id);
+          return { show };
+        }}
+      />
+    </Routes>
   );
 }
 
