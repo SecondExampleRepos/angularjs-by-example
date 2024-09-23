fb8c91e11a0752f92cc8c1fdffa5743e090168f9
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-22 19:44:19 -0700

    edit_file
    # Path: [6. Combine Routes into a single file and add to App.tsx]/edit_file
    # UUID: 4c9cc755-4125-4062-abea-4a8eff4552c7
    
    # Changed files:
    react/src/App.tsx
    - 106fd1a0e9b539074bf0273c92b6a4f2d02d67a5
    - 106fd1a0e9b539074bf0273c92b6a4f2d02d67a5
    - 44f58acd3bad1caee4465afe5e141f0b30b28f9f

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,10 +1,39 @@
+// Converted from src/app.routes.js
+
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Home from '../../components/controllers/home';
+import Premieres from '../../components/controllers/premieres';
+import Search from '../../components/controllers/search';
+import Popular from '../../components/controllers/popular';
+import View from '../../components/controllers/view';
+import ShowService from '../../services/showService';
 
 function App() {
   return (
-    <div>
-      {/* App content goes here */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/premieres"
+          element={<Premieres shows={ShowService.getPremieres()} />}
+        />
+        <Route path="/search" element={<Search />} />
+        <Route
+          path="/search/:query"
+          element={<Search />}
+        />
+        <Route
+          path="/popular"
+          element={<Popular shows={ShowService.getPopular()} />}
+        />
+        <Route
+          path="/view/:id"
+          element={<View />}
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 
