a883385e2bfd63eb4b14e296933dd391374afbb9
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-22 18:52:56 -0700

    edit_file
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_4/async_each/edit_file
    # UUID: eabfbf32-24c3-4210-bc2e-abdc3a18e3a8
    
    # Changed files:
    react/src/services/ShowService.tsx

---
+++ react/src/services/ShowService.tsx
@@ -0,0 +1,86 @@
+// Converted from src/services/show.fct.js
+
+import axios from 'axios';
+import moment from 'moment';
+
+const API_KEY = '87de9079e74c828116acce677f6f255b';
+const BASE_URL = 'http://api.themoviedb.org/3';
+
+type ShowType = {
+    id: number;
+    name: string;
+};
+
+type Genre = {
+    id: number;
+    name: string;
+};
+
+type CastMember = {
+    name: string;
+};
+
+type ShowDetails = {
+    id: number;
+    original_name: string;
+    genres?: Genre[];
+    cast?: CastMember[];
+};
+
+const makeRequest = async (url: string, params: Record<string, any>) => {
+    const requestUrl = `${BASE_URL}/${url}?api_key=${API_KEY}`;
+    const queryString = new URLSearchParams(params).toString();
+    
+    try {
+        const response = await axios.get(`${requestUrl}&${queryString}`, {
+            headers: { 'Content-Type': 'application/json' },
+            cache: true
+        });
+        return response.data;
+    } catch (error) {
+        console.error('XHR Failed for ShowService', error);
+        throw error;
+    }
+};
+
+const getPremieres = async (): Promise<ShowType[]> => {
+    const date = new Date();
+    date.setDate(1);
+    
+    const data = await makeRequest('discover/tv', { 
+        'first_air_date.gte': moment(date).format('DD-MM-YYYY'), 
+        append_to_response: 'genres' 
+    });
+    
+    return data.results;
+};
+
+const getShowDetails = async (id: number): Promise<ShowDetails> => {
+    return await makeRequest(`tv/${id}`, {});
+};
+
+const getCast = async (id: number): Promise<CastMember[]> => {
+    const data = await makeRequest(`tv/${id}/credits`, {});
+    
+    return data.cast;
+};
+
+const searchShows = async (query: string): Promise<ShowType[]> => {
+    const data = await makeRequest('search/tv', { query });
+    
+    return data.results;
+};
+
+const getPopularShows = async (): Promise<ShowType[]> => {
+    const data = await makeRequest('tv/popular', {});
+    
+    return data.results;
+};
+
+export default {
+    getPremieres,
+    getShowDetails,
+    searchShows,
+    getPopularShows,
+    getCast
+};
