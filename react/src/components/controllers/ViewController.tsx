4f31067f2dd264b073e86d17a48961ea2d8a8e0a
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-24 02:44:55 -0700

    edit_file_long_context
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_7/async_each/edit_file_long_context
    # UUID: a1be1a4e-11e9-4ded-a77d-0461fc80b02a
    
    # Changed files:
    react/src/components/controllers/ViewController.tsx

---
+++ react/src/components/controllers/ViewController.tsx
@@ -0,0 +1,45 @@
+// Converted from src/sections/view/view.ctrl.js
+
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import ShowService from '../../services/show.fct';
+
+interface Show {
+  id: number;
+  original_name: string;
+  cast: Array<any>;
+}
+
+interface ViewControllerProps {
+  show: Show;
+}
+
+const ViewController: React.FC<ViewControllerProps> = ({ show }) => {
+  const [cast, setCast] = useState<Array<any>>([]);
+  const location = useLocation();
+
+  useEffect(() => {
+    document.title = "VIEW";
+    // Assuming PageValues.description is used for setting meta description or similar
+    const description = `Overview, seasons & info for '${show.original_name}'.`;
+    // Set meta description or similar here if needed
+
+    ShowService.getCast(show.id).then(response => {
+      setCast(response.cast);
+    });
+  }, [show]);
+
+  const setBannerImage = () => ({
+    background: 'url() no-repeat',
+    backgroundSize: '100%',
+    backgroundPosition: '100% 0%',
+  });
+
+  return (
+    <div style={setBannerImage()}>
+      {/* Render show details and cast here */}
+    </div>
+  );
+};
+
+export default ViewController;
