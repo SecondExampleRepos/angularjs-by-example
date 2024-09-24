830d3afe03acb4bf7220babe80d7152589127e87
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-24 02:44:06 -0700

    edit_file_long_context
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_3/async_each/edit_file_long_context
    # UUID: 9b2cdf93-d4ec-48d6-9abb-3bc764a5d325
    
    # Changed files:
    react/src/utils/filters/characters.ts

---
+++ react/src/utils/filters/characters.ts
@@ -0,0 +1,30 @@
+// Converted from src/assets/js/angular-truncate.js
+
+/**
+ * Truncates a string to a specified number of characters, optionally breaking on the last word.
+ * @param input - The input string to be truncated.
+ * @param chars - The number of characters to truncate to.
+ * @param breakOnWord - Whether to break on the last word or not.
+ * @returns The truncated string with ellipsis if applicable.
+ */
+export function truncateCharacters(input: string, chars: number, breakOnWord: boolean = false): string {
+    if (isNaN(chars)) return input;
+    if (chars <= 0) return '';
+    if (input && input.length > chars) {
+        input = input.substring(0, chars);
+
+        if (!breakOnWord) {
+            const lastspace = input.lastIndexOf(' ');
+            // Get last space
+            if (lastspace !== -1) {
+                input = input.substr(0, lastspace);
+            }
+        } else {
+            while (input.charAt(input.length - 1) === ' ') {
+                input = input.substr(0, input.length - 1);
+            }
+        }
+        return input + '...';
+    }
+    return input;
+}
