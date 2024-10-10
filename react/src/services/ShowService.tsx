47d77f05f688a3454fa3b43b547f288e6f670718
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-09 18:58:16 -0700

    edit_file_long_context
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_4/async_each/edit_file_long_context
    # UUID: caf2d54d-832c-4b55-84f8-cd869f3dc75d
    
    # Changed files:
    react/src/services/ShowService.tsx

---
+++ react/src/services/ShowService.tsx
@@ -0,0 +1,60 @@
+// Converted from src/services/show.fct.js
+
+import axios from 'axios';
+import moment from 'moment';
+
+const API_KEY = '87de9079e74c828116acce677f6f255b';
+const BASE_URL = 'http://api.themoviedb.org/3';
+
+const makeRequest = async (url: string, params: Record<string, string | number>) => {
+    const requestUrl = `${BASE_URL}/${url}?api_key=${API_KEY}&${new URLSearchParams(params).toString()}`;
+    try {
+        const response = await axios.get(requestUrl, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            cache: true
+        });
+        return response.data;
+    } catch (error) {
+        console.error('XHR Failed for ShowService', error);
+        throw error;
+    }
+};
+
+const getPremieres = async () => {
+    const date = new Date();
+    date.setDate(1);
+    const params = {
+        'first_air_date.gte': moment(date).format('DD-MM-YYYY'),
+        append_to_response: 'genres'
+    };
+    const data = await makeRequest('discover/tv', params);
+    return data.results;
+};
+
+const get = async (id: string) => {
+    return await makeRequest(`tv/${id}`, {});
+};
+
+const getCast = async (id: string) => {
+    return await makeRequest(`tv/${id}/credits`, {});
+};
+
+const search = async (query: string) => {
+    const data = await makeRequest('search/tv', { query });
+    return data.results;
+};
+
+const getPopular = async () => {
+    const data = await makeRequest('tv/popular', {});
+    return data.results;
+};
+
+export default {
+    getPremieres,
+    get,
+    search,
+    getPopular,
+    getCast
+};
