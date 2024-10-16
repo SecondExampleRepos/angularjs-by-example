ffc44324db97590a34d8f977218c189b7c7145fd
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-16 20:47:04 +0000

    edit_file_long_context
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_6/async_each/edit_file_long_context
    # UUID: ccc31a69-a68f-4705-939d-aa9c0d0dd07c
    
    # Changed files:
    angular/src/app/directives/show.directive.ts

---
+++ angular/src/app/directives/show.directive.ts
@@ -0,0 +1,23 @@
+// Converted from src/components/show/show.drct.js
+
+import { Directive, Input, OnInit } from '@angular/core';
+import { ShowService } from '../services/show.service';
+
+@Directive({
+  selector: 'app-show',
+  templateUrl: './show.component.html'
+})
+export class ShowDirective implements OnInit {
+  @Input() show: any;
+  genres: string[] = [];
+
+  constructor(private showService: ShowService) {}
+
+  ngOnInit(): void {
+    if (this.show && this.show.id) {
+      this.showService.get(this.show.id).subscribe(response => {
+        this.genres = response.genres;
+      });
+    }
+  }
+}
