428ddef9375f9e8875841d10fda3e55cf5a60416
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-16 11:15:03 -0700

    edit_file_long_context
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_2/async_each/edit_file_long_context
    # UUID: 9d2aeb7b-30df-4784-9e40-4cdc8bbdf7e2
    
    # Changed files:
    angular/src/app/services/API_KEY.ts

---
+++ angular/src/app/services/API_KEY.ts
@@ -0,0 +1,11 @@
+// Converted from src/services/show.fct.js
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiKeyService {
+  readonly API_KEY: string = '87de9079e74c828116acce677f6f255b';
+  readonly BASE_URL: string = 'http://api.themoviedb.org/3';
+}
