f5273d8e919ba5039fba6d237a5061b58f8fe979
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-17 02:23:46 -0700

    edit_file_long_context
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_5/async_each/edit_file_long_context
    # UUID: 619ef46d-6cba-425e-9807-ee5cd862ce3b
    
    # Changed files:
    angular/src/app/services/ShowService.service.ts

---
+++ angular/src/app/services/ShowService.service.ts
@@ -0,0 +1,70 @@
+// Converted from src/services/show.fct.js
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShowService {
+  private readonly API_KEY = '87de9079e74c828116acce677f6f255b';
+  private readonly BASE_URL = 'http://api.themoviedb.org/3';
+
+  constructor(private http: HttpClient) {}
+
+  private makeRequest(url: string, params: any): Observable<any> {
+    let requestUrl = `${this.BASE_URL}/${url}?api_key=${this.API_KEY}`;
+    for (const key in params) {
+      if (params.hasOwnProperty(key)) {
+        requestUrl += `&${key}=${params[key]}`;
+      }
+    }
+    return this.http.get(requestUrl, {
+      headers: { 'Content-Type': 'application/json' },
+      observe: 'body',
+      responseType: 'json'
+    }).pipe(
+      map(response => response),
+      catchError(this.dataServiceError)
+    );
+  }
+
+  getPremieres(): Observable<any> {
+    const date = new Date();
+    date.setDate(1);
+    return this.makeRequest('discover/tv', {
+      'first_air_date.gte': date.toISOString().split('T')[0],
+      append_to_response: 'genres'
+    }).pipe(
+      map(data => data.results)
+    );
+  }
+
+  get(id: string): Observable<any> {
+    return this.makeRequest(`tv/${id}`, {});
+  }
+
+  getCast(id: string): Observable<any> {
+    return this.makeRequest(`tv/${id}/credits`, {});
+  }
+
+  search(query: string): Observable<any> {
+    return this.makeRequest('search/tv', { query }).pipe(
+      map(data => data.results)
+    );
+  }
+
+  getPopular(): Observable<any> {
+    return this.makeRequest('tv/popular', {}).pipe(
+      map(data => data.results)
+    );
+  }
+
+  private dataServiceError(errorResponse: any): Observable<never> {
+    console.error('XHR Failed for ShowService');
+    console.error(errorResponse);
+    throw errorResponse;
+  }
+}
