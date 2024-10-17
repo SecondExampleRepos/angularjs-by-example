5c2a3cc7fa3b713d55572c1473d843b319879d11
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-17 02:23:25 -0700

    edit_file_long_context
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_6/async_each/edit_file_long_context
    # UUID: 4090a3ef-ee05-4475-85da-5e09bbcdbd14
    
    # Changed files:
    angular/src/app/modules/config.module.ts

---
+++ angular/src/app/modules/config.module.ts
@@ -0,0 +1,57 @@
+// Converted from src/app.routes.js
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeController } from '../components/HomeController/HomeController.component';
+import { PremieresController } from '../components/PremieresController/PremieresController.component';
+import { SearchController } from '../components/SearchController/SearchController.component';
+import { PopularController } from '../components/PopularController/PopularController.component';
+import { ViewController } from '../components/ViewController/ViewController.component';
+import { ShowService } from '../services/show.service';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeController
+  },
+  {
+    path: 'premieres',
+    component: PremieresController,
+    resolve: {
+      shows: () => ShowService.getPremieres()
+    }
+  },
+  {
+    path: 'search',
+    component: SearchController
+  },
+  {
+    path: 'search/:query',
+    component: SearchController
+  },
+  {
+    path: 'popular',
+    component: PopularController,
+    resolve: {
+      shows: () => ShowService.getPopular()
+    }
+  },
+  {
+    path: 'view/:id',
+    component: ViewController,
+    resolve: {
+      show: (route: ActivatedRouteSnapshot) => ShowService.get(route.params['id'])
+    }
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [ShowService]
+})
+export class ConfigModule {}
