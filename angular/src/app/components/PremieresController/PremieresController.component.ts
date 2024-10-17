b694cf9a2ae498b865593d3ee3db7cc7f7112e0c
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-17 02:23:14 -0700

    edit_file_long_context
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_8/async_each/edit_file_long_context
    # UUID: c02b8f90-16cc-41cc-9e7d-a45d6c2cb162
    
    # Changed files:
    angular/src/app/components/PremieresController/PremieresController.component.ts

---
+++ angular/src/app/components/PremieresController/PremieresController.component.ts
@@ -0,0 +1,19 @@
+<!-- Converted from src/components/show/show.tpl.html -->
+<div class="show-frame">
+  <ul class="genres">
+    <li *ngFor="let genre of genres" class="animate-repeat" [ngStyle]="{'background-color': 'rgba(59, 185, 187, ' + genres.length / $index / 5 + ')'}">{{genre.name}}</li>
+  </ul>
+  <img preload-image default-image="assets/images/loading.jpg" fallback-image="assets/images/fallback.jpg" [src]="'http://image.tmdb.org/t/p/w780/' + show.backdrop_path" />
+  <div class="date label label-dark"><span class="icon icon-calendar"></span> {{show.first_air_date | date:'dd-MM-yyyy'}}</div>
+  <h2>{{show.original_name | slice:0:40}}</h2>
+  <div class="inner">
+    <ul class="info">
+      <li class="col-xs-6 rating"><span class="icon icon-heart3"></span> {{show.vote_average}}</li>
+      <li class="col-xs-6 country"><span class="icon icon-earth"></span> <span *ngFor="let country of show.origin_country">{{country}}{{!$last ? ', ' : ''}}</span> <span *ngIf="show.origin_country.length == 0">--</span></li>
+      <div class="clearfix"></div>
+    </ul>
+    <div class="buttons">
+      <a [routerLink]="['/view', show.id]" class="btn btn-info"><span class="icon icon-arrow-right7"></span> View</a>
+    </div>
+  </div>
+</div>
