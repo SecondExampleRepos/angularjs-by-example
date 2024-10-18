96cbc404c8a2038f6d1a360e9898a5bdb277c8b2
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-17 18:19:42 -0700

    edit_file_long_context
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_8/async_each/if_else/if/edit_file_long_context
    # UUID: 6a636788-ae09-4ec0-b484-566358020c08
    
    # Changed files:
    angular/src/app/components/show-c1b5d2dacc.component.ts

---
+++ angular/src/app/components/show-c1b5d2dacc.component.ts
@@ -0,0 +1,22 @@
+// Converted from src/components/show/show.drct.js
+
+import { Component, Input, OnInit } from '@angular/core';
+import { ShowService } from '../services/show.service';
+
+@Component({
+  selector: 'app-show',
+  templateUrl: './show.tpl-3e2ab2bde3.html',
+  styleUrls: ['./show.css']
+})
+export class ShowComponent implements OnInit {
+  @Input() show!: any;
+  genres: any[] = [];
+
+  constructor(private showService: ShowService) {}
+
+  ngOnInit(): void {
+    this.showService.get(this.show.id).subscribe(response => {
+      this.genres = response.genres;
+    });
+  }
+}
