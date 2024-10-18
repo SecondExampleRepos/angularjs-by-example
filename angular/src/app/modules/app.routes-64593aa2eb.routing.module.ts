a3e4bc5126e54dd3460c381bf4cd4814adabe719
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-17 17:07:19 -0700

    edit_file
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_6/edit_file
    # UUID: aca06dd6-0333-43c9-9f1a-ad97f7137f5b
    
    # Changed files:
    angular/src/app/modules/app.routes-64593aa2eb.routing.module.ts

---
+++ angular/src/app/modules/app.routes-64593aa2eb.routing.module.ts
@@ -0,0 +1,62 @@
+// Converted from src/app.routes.js
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../components/home.component';
+import { PremieresComponent } from '../components/premieres.component';
+import { SearchComponent } from '../components/search.component';
+import { PopularComponent } from '../components/popular.component';
+import { ViewComponent } from '../components/view.component';
+import { ShowService } from '../services/show.service';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    data: { title: 'HOME', description: 'Learn AngularJS using best practice real world examples.' }
+  },
+  {
+    path: 'premieres',
+    component: PremieresComponent,
+    resolve: {
+      shows: ShowService.prototype.getPremieres
+    },
+    data: { title: 'PREMIERES', description: 'Brand new shows showing this month.' }
+  },
+  {
+    path: 'search',
+    component: SearchComponent,
+    data: { title: 'SEARCH', description: 'Search for your favorite TV shows.' }
+  },
+  {
+    path: 'search/:query',
+    component: SearchComponent,
+    data: { title: 'SEARCH', description: 'Search for your favorite TV shows.' }
+  },
+  {
+    path: 'popular',
+    component: PopularComponent,
+    resolve: {
+      shows: ShowService.prototype.getPopular
+    },
+    data: { title: 'POPULAR', description: 'The most popular TV shows.' }
+  },
+  {
+    path: 'view/:id',
+    component: ViewComponent,
+    resolve: {
+      show: ShowService.prototype.get
+    },
+    data: { title: 'VIEW', description: 'Overview, seasons & info for the show.' }
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
