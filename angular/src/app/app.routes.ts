4ed022462dd0e87ef501c44838e90def4944d629
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-17 18:20:05 -0700

    edit_file
    # Path: [6. Combine Routes into app-routing.module.ts]/edit_file
    # UUID: d20ecb15-539d-491e-8a70-f2f4f19fdadb
    
    # Changed files:
    angular/src/app/app.routes.ts
    - a66801aa066d29329b85e8b4c7fa1c82c4c7d8f8

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,3 +1,49 @@
-import { Routes } from '@angular/router';
+// Converted from angular/src/app/modules/config-64593aa2eb.routing.module.ts
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { HomeComponent } from '../components/home.component';
+import { PremieresComponent } from '../components/premieres.component';
+import { SearchComponent } from '../components/search.component';
+import { PopularComponent } from '../components/popular.component';
+import { ViewComponent } from '../components/view.component';
+import { ShowService } from '../services/show.service';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'premieres',
+    component: PremieresComponent,
+    resolve: {
+      shows: () => ShowService.getPremieres()
+    }
+  },
+  {
+    path: 'search',
+    component: SearchComponent
+  },
+  {
+    path: 'search/:query',
+    component: SearchComponent
+  },
+  {
+    path: 'popular',
+    component: PopularComponent,
+    resolve: {
+      shows: () => ShowService.getPopular()
+    }
+  },
+  {
+    path: 'view/:id',
+    component: ViewComponent,
+    resolve: {
+      show: (route) => ShowService.get(route.params.id)
+    }
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
