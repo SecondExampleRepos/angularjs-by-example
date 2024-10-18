c6ed08920b6a96557f05914c0ba18975ea2b38b6
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-17 20:05:08 -0700

    edit_file_long_context
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_8/async_each/if_else/else/edit_file_long_context
    # UUID: a54c1046-b3a7-48cf-aef8-8d61aa08f313
    
    # Changed files:
    angular/src/app/directives/ngEnter-11f3206ce0.directive.ts

---
+++ angular/src/app/directives/ngEnter-11f3206ce0.directive.ts
@@ -0,0 +1,23 @@
+// Converted from src/directives/ngEnter.drct.js
+
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[ngEnter]'
+})
+export class NgEnterDirective {
+  @Input() ngEnter!: () => void;
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener('keydown', ['$event'])
+  @HostListener('keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent): void {
+    if (event.which === 13) {
+      if (this.ngEnter) {
+        this.ngEnter();
+      }
+      event.preventDefault();
+    }
+  }
+}
