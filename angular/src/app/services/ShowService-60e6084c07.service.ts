1c7b9abd5aa09ef451a268fe94ce6ac4282c3658
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-17 17:07:42 -0700

    edit_file_long_context
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_5/async_each/edit_file_long_context
    # UUID: 75dcc09e-e786-461b-a730-14a82fd1dac6
    
    # Changed files:
    angular/src/app/services/ShowService-60e6084c07.service.ts

---
+++ angular/src/app/services/ShowService-60e6084c07.service.ts
@@ -0,0 +1,74 @@
+// Converted from src/services/show.fct.js
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import * as moment from 'moment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShowService {
+  private readonly API_KEY = '87de9079e74c828116acce677f6f255b';
+  private readonly BASE_URL = 'http://api.themoviedb.org/3';
+
+  constructor(private http: HttpClient) {}
+
+  private makeRequest(url: string, params: any) {
+    let requestUrl = `${this.BASE_URL}/${url}?api_key=${this.API_KEY}`;
+    for (const key in params) {
+      if (params.hasOwnProperty(key)) {
+        requestUrl += `&${key}=${params[key]}`;
+      }
+    }
+    return this.http.get(requestUrl, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).pipe(
+      catchError(this.dataServiceError)
+    );
+  }
+
+  getPremieres() {
+    const date = new Date();
+    date.setDate(1);
+    return this.makeRequest('discover/tv', {
+      'first_air_date.gte': moment(date).format('DD-MM-YYYY'),
+      append_to_response: 'genres'
+    }).pipe(
+      catchError(this.dataServiceError)
+    );
+  }
+
+  get(id: string) {
+    return this.makeRequest(`tv/${id}`, {}).pipe(
+      catchError(this.dataServiceError)
+    );
+  }
+
+  getCast(id: string) {
+    return this.makeRequest(`tv/${id}/credits`, {}).pipe(
+      catchError(this.dataServiceError)
+    );
+  }
+
+  search(query: string) {
+    return this.makeRequest('search/tv', { query }).pipe(
+      catchError(this.dataServiceError)
+    );
+  }
+
+  getPopular() {
+    return this.makeRequest('tv/popular', {}).pipe(
+      catchError(this.dataServiceError)
+    );
+  }
+
+  private dataServiceError(errorResponse: any) {
+    console.error('XHR Failed for ShowService');
+    console.error(errorResponse);
+    return of(errorResponse);
+  }
+}
