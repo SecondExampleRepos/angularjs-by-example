005702c2be3bbdee69a5017d82b93f4a1e637b77
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-17 18:07:28 -0700

    edit_file_long_context
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_2/edit_file_long_context
    # UUID: d8217a96-747a-4982-8a26-c45db62364e8
    
    # Changed files:
    angular/src/main.ts
    - 416573e8df0bc79aafb77b0ae25506aa5a182c0e

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,6 +1,44 @@
+// Converted from src/app.js
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AppRoutes } from './app/app.routes';
+import { BarComponent } from './app/components/bar/bar.component';
+import { ShowDirective } from './app/directives/show.directive';
+import { NgEnterDirective } from './app/directives/ng-enter.directive';
+import { PageValuesService } from './app/services/page-values.service';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+@NgModule({
+  declarations: [
+    AppComponent,
+    BarComponent,
+    ShowDirective,
+    NgEnterDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(AppRoutes)
+  ],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    PageValuesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(AppRoutes),
+    provideHttpClient(withInterceptorsFromDi())
+  ]
+}).catch((err) => console.error(err));
