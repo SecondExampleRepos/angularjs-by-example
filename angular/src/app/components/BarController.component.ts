8e27887c250419c606b96cd98ffb869a57b16223
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-22 23:42:52 -0700

    edit_file_long_context
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_9/async_each/edit_file_long_context
    # UUID: 954143c7-2d01-4555-a1dc-a11f22240b87
    
    # Changed files:
    angular/src/app/components/BarController.component.ts

---
+++ angular/src/app/components/BarController.component.ts
@@ -0,0 +1,17 @@
+// Converted from src/components/bar/bar.ctrl.js
+
+import { Component, Input } from '@angular/core';
+import { PageValues } from '../../constants/PageValues';
+
+@Component({
+  selector: 'app-bar-controller',
+  templateUrl: './bar-controller.component.html',
+  styleUrls: ['./bar-controller.component.css']
+})
+export class BarControllerComponent {
+  @Input() data!: typeof PageValues;
+
+  constructor() {
+    // Initialize data if necessary
+  }
+}
