eb77ccef6420eedf7f2fb9abdc83151f41f3234e
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-10-22 17:02:27 -0700

    edit_file_long_context
    # Path: [5. Convert files in order]/for_each/async_each/switch/case_9/async_each/edit_file_long_context
    # UUID: fd97e75a-91d3-40b4-92e4-169d0142a261
    
    # Changed files:
    angular/src/app/components/ViewController.component.ts

---
+++ angular/src/app/components/ViewController.component.ts
@@ -0,0 +1,49 @@
+// Converted from src/sections/view/view.ctrl.js
+
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ShowService } from '../../services/show.service';
+import { PageValues } from '../../services/page.val';
+
+@Component({
+  selector: 'app-view-controller',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.css']
+})
+export class ViewControllerComponent implements OnInit {
+  @Input() show!: any;
+  cast: any[] = [];
+
+  constructor(
+    private showService: ShowService,
+    private route: ActivatedRoute,
+    private pageValues: PageValues
+  ) {}
+
+  ngOnInit(): void {
+    this.pageValues.title = "VIEW";
+    this.pageValues.description = `Overview, seasons & info for '${this.show.original_name}'.`;
+
+    this.route.params.subscribe(params => {
+      const showId = params['id'];
+      this.showService.get(showId).subscribe(show => {
+        this.show = show;
+        this.loadCast(showId);
+      });
+    });
+  }
+
+  setBannerImage() {
+    return {
+      'background': 'url() no-repeat',
+      'background-size': '100%',
+      'background-position': '100% 0%'
+    };
+  }
+
+  private loadCast(showId: string): void {
+    this.showService.getCast(showId).subscribe(response => {
+      this.cast = response.cast;
+    });
+  }
+}
